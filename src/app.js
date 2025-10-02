be069b633747660c678b19bbf25a2e32ac4de87d
andre <andre.martins@unigran.br>
2025-10-02 19:34:14 -0400

    Testando Ci

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/api', productRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
