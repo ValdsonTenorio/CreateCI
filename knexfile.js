9198f873c27192e8801b690d9d3c25337df9e4c9
Valdson <valdsonmartinstenorio721@gmail.com>
2025-10-09 12:07:15 -0400

    mudancas no knex

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,26 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST || 'db',
+      host: process.env.DB_HOST || 'db', 
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_DATABASE || 'productdb',
       user: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres'
+    },
+    migrations: {
+      directory: './migrations'
+    }
+  },
+
+  
+  test: {
+    client: 'pg', 
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     },
     migrations: {
       directory: './migrations'
